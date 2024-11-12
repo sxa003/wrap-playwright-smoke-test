57845c17ddf32b6f0074dccb3cffe17475713a08
sxm101 <sopyan.mulyana@insigniafinancial.com.au>
2024-11-12 14:22:41 +1100

    DDOFSC-61 delete some files

--- tests/test-2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-
-  await page.getByLabel('Search', { exact: true }).click();
-await page.getByLabel('Search', { exact: true }).fill('test somthing');
-});
